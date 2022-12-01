fcd992552491ce62c08978c6660849ac0afaa338
Joel <joelangeltw@gmail.com>
2022-12-01 15:36:13 -0500

    added layout component

---
+++ components/Layout/Layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+  class?: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="mx-auto h-full w-full max-w-[1312px] px-4 md:w-[calc(100%-3rem)] lg:w-[calc(100%-100px)] lg:px-0"
+    :class="props.class"
+  >
+    <slot />
+  </div>
+</template>

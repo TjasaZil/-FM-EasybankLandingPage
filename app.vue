8facd055d485970f253cbd640ef93b47e84ab0d6
Joel <joelangeltw@gmail.com>
2022-12-01 12:55:31 -0500

    footer component

--- app.vue
+++ app.vue
@@ -2,4 +2,8 @@
   <div>
     <p class="text-3xl font-extrabold text-blue-900">Home page</p>
   </div>
+  <Footer />
 </template>
+<script>
+import Footer from './components/Footer/Footer.vue';
+</script>

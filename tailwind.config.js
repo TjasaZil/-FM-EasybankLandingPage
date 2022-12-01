d7da9af802950a99d78ee5031845479742fb7f96
Joel <joelangeltw@gmail.com>
2022-12-01 11:08:22 -0500

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
     './app.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          'dark-blue': 'hsl(233, 26%, 24%)',
+          'lime-green': 'hsl(136, 65%, 51%)',
+          'bright-cyan': 'hsl(192, 70%, 51%)',
+        },
+        neutral: {
+          'grayish-blue': 'hsl(233, 8%, 62%)',
+          'light-grayish-blue': 'hsl(220, 16%, 96%)',
+          'very-light-gray': 'hsl(0, 0%, 98%)',
+          white: 'hsl(0, 0%, 100%)',
+        },
+      },
+    },
   },
 };
